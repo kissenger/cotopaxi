ddabcc86ce960755d450dee06ab7d153c846b549
Gordon Taylor <ivyterrace@hotmail.co.uk>
2020-05-20 11:52:25 +0100

    find backend through environment files

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  BACKEND_HOST: 'localhost',
+  BACKEND_PORT: 3000,
 };
 
 /*
